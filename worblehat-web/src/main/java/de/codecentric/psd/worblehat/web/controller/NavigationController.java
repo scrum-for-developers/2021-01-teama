23cc3499a19151f00080bfb2b31476d47b8ae1da
Christian Schmidt <christian.schmidt@tenado.de>
2021-01-21 12:30:47 +0100

    homepage shows table

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
@@ -1,15 +1,28 @@
 package de.codecentric.psd.worblehat.web.controller;
 
+import de.codecentric.psd.worblehat.domain.Book;
+import de.codecentric.psd.worblehat.domain.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /** Controller for Navigation */
 @Controller
 public class NavigationController {
+  private BookService bookService;
+
+  @Autowired
+  public NavigationController(BookService bookService) {
+    this.bookService = bookService;
+  }
 
   @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String home() {
+  public String home(ModelMap modelMap) {
+    List<Book> books = bookService.findAllBooks();
+    modelMap.addAttribute("books", books);
     return "home";
   }
 }
