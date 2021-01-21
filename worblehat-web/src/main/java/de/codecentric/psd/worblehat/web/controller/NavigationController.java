42dc051b4fe19cb37db4a5491f7734cedac9b1d4
Christian Schmidt <christian.schmidt@tenado.de>
2021-01-21 14:05:28 +0100

    testy questy fixie for navigatione

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/NavigationController.java
@@ -1,28 +1,22 @@
 package de.codecentric.psd.worblehat.web.controller;
 
-import de.codecentric.psd.worblehat.domain.Book;
-import de.codecentric.psd.worblehat.domain.BookService;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /** Controller for Navigation */
 @Controller
 public class NavigationController {
-  private BookService bookService;
+  // private BookService bookService;
 
-  @Autowired
-  public NavigationController(BookService bookService) {
-    this.bookService = bookService;
+  // @Autowired
+  public NavigationController() {
+    // this.bookService = bookService;
   }
 
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String home(ModelMap modelMap) {
-    List<Book> books = bookService.findAllBooks();
-    modelMap.addAttribute("books", books);
+  @RequestMapping(value = "/" /*, method = RequestMethod.GET*/)
+  public String home(/*ModelMap modelMap*/ ) {
+    // List<Book> books = bookService.findAllBooks();
+    // modelMap.addAttribute("books", books);
     return "home";
   }
 }
